f58a4d3bd9b69a845d2a42646d56e5bb9d70a633
Arav Bhivgade <bhivgadearav@gmail.com>
2026-06-20 17:23:44 +0530

    made solution #1

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,66 +6,144 @@ import "./IDividends.sol";
 import "./SafeMath.sol";
 
 contract Token is IERC20, IMintableToken, IDividends {
-  // ------------------------------------------ //
-  // ----- BEGIN: DO NOT EDIT THIS SECTION ---- //
-  // ------------------------------------------ //
-  using SafeMath for uint256;
-  uint256 public totalSupply;
-  uint256 public decimals = 18;
-  string public name = "Test token";
-  string public symbol = "TEST";
-  mapping (address => uint256) public balanceOf;
-  // ------------------------------------------ //
-  // ----- END: DO NOT EDIT THIS SECTION ------ //  
-  // ------------------------------------------ //
+    // ------------------------------------------ //
+    // ----- BEGIN: DO NOT EDIT THIS SECTION ---- //
+    // ------------------------------------------ //
+    using SafeMath for uint256;
+    uint256 public totalSupply;
+    uint256 public decimals = 18;
+    string public name = "Test token";
+    string public symbol = "TEST";
+    mapping(address => uint256) public balanceOf;
 
-  // IERC20
+    // ------------------------------------------ //
+    // ----- END: DO NOT EDIT THIS SECTION ------ //
+    // ------------------------------------------ //
 
-  function allowance(address owner, address spender) external view override returns (uint256) {
-    revert();
-  }
+    mapping(address => bool) public participant;
+    mapping(address => uint256) public index;
+    address[] public tokenHolders;
+    mapping(address account => mapping(address spender => uint256))
+        private allowances;
+    uint256 public dividends;
 
-  function transfer(address to, uint256 value) external override returns (bool) {
-    revert();
-  }
+    // IERC20
 
-  function approve(address spender, uint256 value) external override returns (bool) {
-    revert();
-  }
+    function allowance(
+        address owner,
+        address spender
+    ) external view override returns (uint256) {
+        return allowances[owner][spender];
+    }
 
-  function transferFrom(address from, address to, uint256 value) external override returns (bool) {
-    revert();
-  }
+    function transfer(
+        address to,
+        uint256 value
+    ) external override returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        if (!participant[to]) {
+            tokenHolders.push(to);
+            index[to] = tokenHolders.length - 1;
+            participant[to] = true;
+        }
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(value);
+        if (balanceOf[msg.sender] == 0) {
+            uint256 index = index[msg.sender];
+            delete tokenHolders[index];
+        }
+        balanceOf[to] = balanceOf[to].add(value);
+        return true;
+    }
 
-  // IMintableToken
+    function approve(
+        address spender,
+        uint256 value
+    ) external override returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Insufficient balance");
+        allowances[msg.sender][spender] = value;
+        return true;
+    }
 
-  function mint() external payable override {
-    revert();
-  }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external override returns (bool) {
+        require(balanceOf[from] >= value, "Insufficient balance");
+        require(
+            allowances[from][msg.sender] >= value,
+            "Insufficient allowance"
+        );
+        if (!participant[to]) {
+            tokenHolders.push(to);
+            index[to] = tokenHolders.length - 1;
+            participant[to] = true;
+        }
+        balanceOf[from] = balanceOf[from].sub(value);
+        if (balanceOf[from] == 0) {
+            uint256 index = index[from];
+            delete tokenHolders[index];
+        }
+        balanceOf[to] = balanceOf[to].add(value);
+        allowances[from][msg.sender] = allowances[from][msg.sender].sub(value);
+        return true;
+    }
 
-  function burn(address payable dest) external override {
-    revert();
-  }
+    // IMintableToken
 
-  // IDividends
+    function mint() external payable override {
+        require(msg.value > 0, "Invalid value");
+        if (!participant[msg.sender]) {
+            tokenHolders.push(msg.sender);
+            index[msg.sender] = tokenHolders.length - 1;
+        }
+        participant[msg.sender] = true;
+        balanceOf[msg.sender] = balanceOf[msg.sender].add(msg.value);
+        totalSupply = totalSupply.add(msg.value);
+    }
 
-  function getNumTokenHolders() external view override returns (uint256) {
-    revert();
-  }
+    function burn(address payable dest) external override {
+        uint256 index = index[msg.sender];
+        delete tokenHolders[index];
+        uint256 balance = balanceOf[msg.sender];
+        balanceOf[msg.sender] = 0;
+        totalSupply = totalSupply.sub(balance);
+        (bool sent, ) = dest.call{value: balance}("");
+        require(sent, "Failed to send ETH");
+    }
 
-  function getTokenHolder(uint256 index) external view override returns (address) {
-    revert();
-  }
+    // IDividends
 
-  function recordDividend() external payable override {
-    revert();
-  }
+    function getNumTokenHolders() external view override returns (uint256) {
+        return tokenHolders.length;
+    }
 
-  function getWithdrawableDividend(address payee) external view override returns (uint256) {
-    revert();
-  }
+    function getTokenHolder(
+        uint256 index
+    ) external view override returns (address) {
+        return tokenHolders[index];
+    }
 
-  function withdrawDividend(address payable dest) external override {
-    revert();
-  }
+    function recordDividend() external payable override {
+        require(msg.value > 0, "Invalid value");
+        dividends = dividends.add(msg.value);
+    }
+
+    function getWithdrawableDividend(
+        address payee
+    ) external view override returns (uint256) {
+        uint256 balance = balanceOf[payee];
+        uint256 totalEth = dividends;
+        uint256 ethValue = totalEth.div(totalSupply);
+        return balance.mul(ethValue);
+    }
+
+    function withdrawDividend(address payable dest) external override {
+        require(balanceOf[msg.sender] > 0, "Insufficient balance");
+        uint256 tokenValue = dividends.div(totalSupply);
+        uint256 withdrawableDividend = balanceOf[msg.sender].mul(tokenValue);
+        (bool sent, ) = dest.call{value: withdrawableDividend}("");
+        require(sent, "Failed to send ETH");
+        dividends = dividends.sub(withdrawableDividend);
+    }
 }
